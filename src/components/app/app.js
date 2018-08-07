fbb4da6d3870db0254a391d4734a711ab3775beb
Ariel Altaras <ariel.altaras@gmail.com>
2018-08-06 19:50:18 -0700

    added landing rout and got function rendering

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,22 +1,17 @@
 import React from 'react';
 import {BrowserRouter,Route} from 'react-router-dom';
 
-import Dashboard from './dashboard/dashboard.js';
-import Landing from './landing/landing.js';
-import NoteCreateForm from './note-create-form/note-create-form.js';
-import NoteList from './note-list/note-list.js';
-import NoteItem from './note-item/note-item.js';
-import '../style/main.scss';
+import Landing from '../landing/landing.js';
+// import Dashboard from '../dashboard/dashboard.js';
+// import NoteCreateForm from '../note-create-form/note-create-form.js';
+// import NoteList from '../note-list/note-list.js';
+// import NoteItem from '../note-item/note-item.js';
+import '../../style/main.scss';
 
 export default class App extends React.Component {
 
   constructor(props) {
     super(props);
-
-    this.state = {
-    };
-
-    this.addItem = this.addItem.bind(this);
 
   }
 
@@ -26,10 +21,9 @@ export default class App extends React.Component {
     return (
       <BrowserRouter>
         <React.Fragment>
-          <Header title="Note App" />
           <main>
             <Route exact path="/" component={Landing} />
-            <Route exact path="/dashboard" component={Dashboard}/>
+            {/* <Route exact path="/dashboard" component={Dashboard}/> */}
           </main>
         </React.Fragment>
       </BrowserRouter>
